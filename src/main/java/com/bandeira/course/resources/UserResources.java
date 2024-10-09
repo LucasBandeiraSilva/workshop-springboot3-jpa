3ef7098cd3ae6bf712e4fc167610013c77e9d3e9
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-10-09 15:41:48 -0300

    Feat: Service layer, component registration

--- src/main/java/com/bandeira/course/resources/UserResources.java
+++ src/main/java/com/bandeira/course/resources/UserResources.java
@@ -1,17 +1,28 @@
 package com.bandeira.course.resources;
 
 import com.bandeira.course.entities.User;
+import com.bandeira.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User>findAll(){
-        User user = new User(1L,"Lucas","lucas@gmail.com","999999-99","a1b2c5b7");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>>findAll(){
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User>findById( @PathVariable Long id ){
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }

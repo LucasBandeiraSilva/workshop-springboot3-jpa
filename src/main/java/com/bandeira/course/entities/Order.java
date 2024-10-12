6094e1c00051749ed44041128640494355c16ad2
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-10-12 07:21:21 -0300

    Feat: Payment, one-to-one association

--- src/main/java/com/bandeira/course/entities/Order.java
+++ src/main/java/com/bandeira/course/entities/Order.java
@@ -22,6 +22,8 @@ public class Order implements Serializable {
     private User client;
     @OneToMany(mappedBy = "id.order")
     private Set <OrderItem> items = new HashSet <>();
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -68,6 +70,14 @@ public class Order implements Serializable {
 
     public Set <OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment( Payment payment ) {
+        this.payment = payment;
     }
 
     @Override
